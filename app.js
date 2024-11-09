da41dd732d139eb1875a5cba9f8ea5c1396af22b
theo_hrt <theo.huret@epitech.eu>
2024-11-09 16:57:41 +0100

    feat(authent): add basic auth

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const basicAuth = require('express-basic-auth');
 
 const app = express();
 const PORT = 3000;
+
+app.use(basicAuth({
+    users: { [process.env.USERNAME]: process.env.PASSWORD },
+    challenge: true,
+    realm: "Restricted Access"
+}));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
